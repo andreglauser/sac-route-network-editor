ccac15bca310cb382c0d93661d5b597a2e1445f0
André Glauser <mail@andreglauser.com>
2024-12-09 08:53:15 +0100

    Restructure schema and build process

--- models/base.sql
+++ models/base.sql
@@ -7,21 +7,20 @@ CREATE TABLE base (
   -- use an autoincrementing integer as the primary key for use in gis applications
   -- some applications may require the first integer field as an unique integer
   fid INTEGER PRIMARY KEY AUTOINCREMENT,
-  -- id is a uuid generated at the time of creation by the application oder user
-  -- all relations from the specialist subject have to use this
-  
-  -- important advantage: relations and references are not broken by changes in the primary key by ogr2ogr when transfering data
-  -- important advantage: different data sources can be merged without conflicts into one database
 
-  -- not set as feault value on database level, to enforce, that it is set by the application/user
-  -- the unique constraint unfortunately prevents the copy for the id during a merge operation in QGIS
+  -- `id` is a uuid generated at the time of creation by the application or the user
+  -- All relations from the  business domain have to use this
+  -- Advantage 1: Different data sources can be merged without conflicts into one database
+  -- Advantage 2: Relations and references are not broken by changes in the primary key by ogr2ogr when transfering data
+  -- Should not be set as deault value on database level. So it can be enforced, that id is set by the application/user
+  -- The unique constraint unfortunately prevents the copy for the id during a merge operation in QGIS
   id TEXT NOT NULL UNIQUE,
   
-  -- theamtic attributes
+  -- thematic attributes
   name TEXT,
   description TEXT,
   
-  -- audit attributes
+  -- audit attributes fro editor tracking. Could be set to NOT NULL to endforce tracking
   created_at TEXT,
   created_by TEXT,
   updated_at TEXT,
---
+++ models/init_db.sql
@@ -0,0 +1,21 @@
+SELECT load_extension('mod_spatialite');
+
+SELECT InitSpatialMetaData(TRUE);
+
+DROP TABLE IF EXISTS config;
+CREATE TABLE config (
+  fid INTEGER PRIMARY KEY AUTOINCREMENT,
+
+  key TEXT UNIQUE,
+  value TEXT,
+  description TEXT,
+  
+  created_at TEXT,
+  created_by TEXT,
+  updated_at TEXT,
+  updated_by TEXT
+);
+
+INSERT INTO config (key, value, description, created_at, created_by) VALUES 
+  ('version', '0.1', 'Version of the database schema', datetime('now'), 'system'),
+  ('execute_triggers', 'true', 'Enable or disable triggers', datetime('now'), 'system');
